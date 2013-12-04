09c2dc51015b2b9427dd690ff71cee4e6f7040ac
peachananr <peachananr@gmail.com>
2013-12-05 00:01:06 +0700

    initial release

--- README.md
+++ README.md
@@ -1,4 +1,89 @@
-flipping_gallery
-================
-
+#Flipping Gallery by Pete R.
 Create a simple but beautiful 3D flipping gallery with on JS call
+Created by [Pete R.](http://www.thepetedesign.com), Founder of [BucketListly](http://www.bucketlistly.com)
+
+[![Flipping Gallery](http://www.thepetedesign.com/images/flipping_gallery_image.png "Flipping Gallery")](http://www.thepetedesign.com/demos/flipping_gallery_demo.html)
+
+## Demo
+[View demo](http://www.thepetedesign.com/demos/flipping_gallery_demo.html)
+
+## Compatibility
+Modern browsers such as Chrome, Firefox, and Safari on both desktop and smartphones have been tested. I have not tested this on IE.
+
+## Basic Usage
+
+Flipping Gallery let you create a simple but beautiful 3D flipping gallery using minimal HTML markups and one JS call.
+
+To add this to your website, make sure you've included the latest jQuery library together with `jquery.flipping_gallery.js` and `flipping_gallery.css` into your document's `<head>`, and make sure your HTML markup are as follows:
+````html  
+<body>
+  ..
+  <div class="gallery">
+    <a href="#"><img src="..."></a>
+    <a href="#"><img src="..."></a>
+    <a href="#"><img src="..."></a>
+    <a href="#"><img src="..."></a>
+    <a href="#"><img src="..."></a>
+    ...
+  </ul>
+  ..
+</body>
+````
+
+And call the JS like this:
+
+````javascript
+  $(".gallery").flipping_gallery({
+    direction: "forward", // This is will set the flipping direction when the gallery is clicked. Options available are "forward", or "backward". The default value is forward.
+    selector: "> a", // This will let you change the default selector which by default, will look for <a> tag and generate the gallery from it. This option accepts normal CSS selectors.
+    spacing: 10, // You can set the spacing between each photo in the gallery here. The number represents the pixels between each photos. The default value is 10.
+    showMaximum: 15, // This will let you limit the number of photos that will be in the viewport. In case you have a gazillion photos, this is perfect to hide all those photos and limit only a few in the viewport.
+    enableScroll: true // Set this to false if you don't want the plugin to override your scrolling behavior. The default value is true.
+  });
+````
+
+## Adding Caption
+
+To add captions, all you have to do is add a `data-caption` attribute to your markup like this:
+
+````html  
+<body>
+  ..
+  <div class="gallery">
+    <a href="#"><img data-caption="..." src="..."></a>
+    <a href="#"><img data-caption="..." src="..."></a>
+    <a href="#"><img data-caption="..." src="..."></a>
+    <a href="#"><img data-caption="..." src="..."></a>
+    <a href="#"><img data-caption="..." src="..."></a>
+    ...
+  </ul>
+  ..
+</body>
+````
+
+Once that is done, call the function normally and your caption will appear below the image as you scroll through the gallery.
+
+
+## Public Method
+
+### $.fn.flipForward()
+
+You can manually trigger the gallery to flip forward to the next photo by calling the method like this:
+
+````javascript
+$(".gallery").flipForward();
+````
+
+### $.fn.flipBackward()
+
+You can manually trigger the gallery to flip backward to the next photo by calling the method like this:
+
+````javascript
+$(".gallery").flipBackward();
+
+Now your website will be equipped with a flipping awesome gallery unlike others. Stay tuned for more updates.
+
+If you want to see more of my plugins, visit [The Pete Design](http://www.thepetedesign.com/#design), or follow me on [Twitter](http://www.twitter.com/peachananr) and [Github](http://www.github.com/peachananr).
+
+## Other Resources
+- Tutorial (Coming Soon)
---
+++ jquery.flipping_gallery.js
@@ -0,0 +1,160 @@
+/* ===========================================================
+ * jquery-flipping_gallery.js v1
+ * ===========================================================
+ * Copyright 2013 Pete Rojwongsuriya.
+ * http://www.thepetedesign.com
+ *
+ * Create a simple but beautiful
+ * 3D flipping gallery with on JS call
+ *
+ * https://github.com/peachananr/flipping_gallery
+ *
+ * ========================================================== */
+
+!function($){
+  
+  var defaults = {
+    direction: "forward",
+    selector: "> a",
+    spacing: 10,
+    showMaximum: 15,
+    enableScroll: true
+	};
+	
+	
+  
+  $.fn.flipping_gallery = function(options){
+    var settings = $.extend({}, defaults, options),
+        el = $(this),
+        total = el.find(settings.selector).length,
+        space = 0,
+        scale = 0.5 / total, 
+        opacity = 1 / total,
+        lastAnimation = 0,
+        quietPeriod = 500;  
+      
+    if (total > settings.showMaximum) opacity = 1 / settings.showMaximum
+    el.addClass("fg-body")
+    
+    
+    $.fn.realignCards = function() {
+      var el = $(this);
+      $.each(el.find(settings.selector), function(i) {
+        if (i == 0) {
+          $(this).addClass("active")
+          el.find(".fg-caption").remove()
+          if($(this).data("caption")) {
+            el.append("<div class='fg-caption' style='opacity: 0;'>" + $(this).attr("data-caption") + "</div>")
+            el.find(".fg-caption").css({
+              "opacity": "1"
+            });
+          }
+        }
+        
+        
+        space = space + settings.spacing
+        new_scale = 1 - (scale * i)
+        new_opacity = 1 - (opacity * i)
+        if (i >= settings.showMaximum) {
+          $(this).css("opacity", 0)
+        } else {
+           $(this).css("opacity", 1)
+        }
+        $(this).addClass("animate fg-card").css({
+          "z-index": 5 - i,
+          "margin-top": "-" + space + "px",
+          "-webkit-transform": "scale(" + new_scale + ")",
+          "-moz-transform": "scale(" + new_scale + ")",
+          "-o-transform": "scale(" + new_scale + ")",
+          "transform": "scale(" + new_scale + ")",
+          "opacity": new_opacity
+        });
+        $(this).click(function() {
+          return false;
+        });
+      });
+      el.find("> .fg-card.active").click(function() {
+        if (settings.direction == "forward") {
+          el.flipForward();
+        } else {
+          el.flipBackward();
+        }
+        
+        return false;
+      })
+    }
+    
+    function init_scroll(event, delta) {
+        deltaOfInterest = delta;
+        var timeNow = new Date().getTime();
+        // Cancel scroll if currently animating or within quiet period
+        if(timeNow - lastAnimation < quietPeriod + settings.animationTime) {
+            event.preventDefault();
+            return;
+        }
+
+        if (deltaOfInterest < 0) {
+          el.flipForward()
+        } else {
+          el.flipBackward()
+        }
+        lastAnimation = timeNow;
+    }
+    
+    $.fn.flipForward = function() {
+      if (!el.hasClass("animating")) {
+        el.addClass("animating")
+        el.find(".fg-caption").remove();
+        var card = el.find("> .fg-card").first();
+        card.addClass("fg-flipping").css("opacity", "0");
+        card.one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function(e) {
+          var save_card = card.removeClass("animate active fg-flipping [class^='fg-count-']").clone();
+          card.remove();
+          el.append(save_card.hide());
+          
+          el.one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function(e) {
+            space = 0;
+            el.realignCards()
+          });
+          
+          el.find("> .fg-card").one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend',function(e) {
+            el.find("> .fg-card").fadeIn()
+            el.removeClass("animating")
+          });
+        });
+      }
+    }
+    
+    $.fn.flipBackward = function() {
+      if (!el.hasClass("animating")) {
+        el.addClass("animating")
+        var prev_card = el.find("> .fg-card").last();
+        var new_prev_card = prev_card.clone()
+        prev_card.remove()
+        el.find(".active").removeClass("active")
+        el.prepend(new_prev_card.attr("style", "").css({
+          "opacity": "0",
+          "z-index": "99"
+        }).hide().addClass("active fg-flipping"))
+        el.find("> .fg-card.active").addClass("animate").show().removeClass("fg-flipping").css("opacity", "1")
+        space = 0;
+        el.realignCards();
+        el.find("> .fg-card").one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend',function(e) {
+        
+          el.removeClass("animating")
+        });
+      }
+    }
+    
+    el.realignCards();
+    
+    if (settings.enableScroll == true) {
+      $(el).bind('mousewheel DOMMouseScroll', function(event) {
+        event.preventDefault();
+        var delta = event.originalEvent.wheelDelta || -event.originalEvent.detail;
+        init_scroll(event, delta);
+      });
+    }
+  }
+}(window.jQuery);
+
